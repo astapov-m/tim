fb03588b1ed2997e45766ca6acb9658b0709ee0a
astapov-m <astapm115@gmail.com>
2022-03-10 17:22:48 +0300

    reset

--- config/filesystems.php
+++ config/filesystems.php
@@ -29,6 +29,10 @@ return [
     */
 
     'disks' => [
+        'reset' => [
+            'driver' => 'local',
+            'root' => base_path('resource/images'),
+        ],
 
         'local' => [
             'driver' => 'local',
